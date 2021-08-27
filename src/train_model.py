ed4e18f320c868477735fefec4dd7f93b857817e
Vyacheslav Khvan <vykhvan@gmail.com>
2021-08-27 06:24:21 +0000

    flake8 formated

--- src/train_model.py
+++ src/train_model.py
@@ -4,7 +4,6 @@ from sklearn.model_selection import train_test_split
 
 # Add the necessary imports for the starter code.
 import pandas as pd
-import logging
 import joblib
 from ml.data import process_data
 from ml.model import train_model
@@ -17,7 +16,8 @@ from ml.model import compute_model_slice_metrics
 print("Data ingestion step")
 data = pd.read_csv("../data/cleaned_data.csv")
 
-# Optional enhancement, use K-fold cross validation instead of a train-test split.
+# Optional enhancement, use K-fold cross validation instead of a
+# train-test split.
 
 cat_features = [
     "workclass",
@@ -34,19 +34,19 @@ train, test = train_test_split(data, test_size=0.3)
 
 print("Data preprocessing step for train set")
 X_train, y_train, encoder, lb = process_data(
-    train, 
-    categorical_features=cat_features, 
-    label="salary", 
+    train,
+    categorical_features=cat_features,
+    label="salary",
     training=True
 )
 
 # Proces the test data with the process_data function.
 print("Data preprocessing step for test set")
-X_test, y_test, encoder, lb  = process_data(
-    test, 
-    categorical_features=cat_features, 
-    label="salary", 
-    training=False, 
+X_test, y_test, encoder, lb = process_data(
+    test,
+    categorical_features=cat_features,
+    label="salary",
+    training=False,
     encoder=encoder,
     lb=lb
 )
@@ -59,7 +59,7 @@ print("Inference step")
 preds = inference(model, X_test)
 
 print("Scoring step")
-precision, recall, fbeta =  compute_model_metrics(y_test, preds)
+precision, recall, fbeta = compute_model_metrics(y_test, preds)
 
 print(f"precision: {precision}")
 print(f"recall: {recall}")
